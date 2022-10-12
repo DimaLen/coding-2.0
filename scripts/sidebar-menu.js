babf2c54d73271d079e811d787e1037aa93211ea
Dima <dimasiklen02@gmail.com>
2022-10-12 18:41:49 +0300

    coding-2.0

---
+++ scripts/sidebar-menu.js
@@ -0,0 +1,12 @@
+const openingBtn = document.querySelector(".sidebar_burger-menu");
+const closingBtn = document.querySelector(".sidebar__close");
+const sidebar = document.querySelector('.sidebar');
+
+
+openingBtn.addEventListener("click", function() {
+  sidebar.classList.add('sidebar-open');
+});
+
+closingBtn.addEventListener('click', function() {
+    sidebar.classList.remove('sidebar-open');
+});
